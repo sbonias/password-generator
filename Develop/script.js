512d337ff0d4f94b18f48dd41ab49529362635e6
Sam Bonias <sbonias84@gmail.com>
2020-04-18 15:54:06 -0400

    applied array join method to convert array to string. Had to do another loop, random(math.floor), array push, array join to drill down to allotted # of characters.

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
@@ -10,9 +13,6 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
 //-------------code above this line was provided--------------
 //Declare variables
 var charLength
@@ -20,12 +20,15 @@ var lowerCase
 var upperCase
 var numValues
 var specialChar
+var finalPassword
 
 //Document Arrays
 var arrLowerCase = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var arrUpperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var arrNumValues = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20"];
 var arrSpecialChar = ["!", "@", "#", "$", "<", ">", "%", "+", "*"];
+var arrPrelimPassword = [];
+var arrRandomPassword = [];
 
 //Input criteria 1 = charLength
 
@@ -43,7 +46,7 @@ var generatePassword = function () {
     console.log(charLength);
   }
 
-  //Input criteria 2-4
+  //Input criteria 2-4 = lowerCase, upperCase, numValues, specialChar
 
   //confirm for lowerCase to yield "true or false" result
   var lowerCase = confirm("Would you like your new password to contain lower case letters? OK = Yes | Cancel = No");
@@ -71,12 +74,11 @@ var generatePassword = function () {
   //create a new variable + IF Stmt to randomly apply a value from the variables corresponding array based on user input (true of false)
   //use "Math.floor(Math.random()" formula for random generation of array values
   //https://www.kirupa.com/html5/picking_random_item_from_array.htm
-  //Placed the IF statements inside a "for loop" so that enough random characters would be generated
+  //Placed the IF statements inside a "for loop" so that the code would run until the charLength requirements are met
   //Number of "true" IF Stmts * charLength = random items generated
   //At this point I noticed that I needed to store the output from the Loop of the IF Stmt to an Array
   //Created "arrPrelimPassword" array and used the special method of "push" to populate this Array with preliminary pw values
 
-  var arrPrelimPassword = [];
   for (var i = 0; i < charLength; i++) {
     if (lowerCase === true) {
       var randomLowerCase = arrLowerCase[Math.floor(Math.random() * arrLowerCase.length)]
@@ -87,20 +89,43 @@ var generatePassword = function () {
       var randomUpperCase = arrUpperCase[Math.floor(Math.random() * arrUpperCase.length)]
       arrPrelimPassword.push(randomUpperCase);
       console.log(randomUpperCase);
+
     }
     if (numValues === true) {
       var randomNumValues = arrNumValues[Math.floor(Math.random() * arrNumValues.length)]
       arrPrelimPassword.push(randomNumValues);
       console.log(randomNumValues);
+
     }
     if (specialChar === true) {
       var randomSpecialChar = arrSpecialChar[Math.floor(Math.random() * arrSpecialChar.length)]
       arrPrelimPassword.push(randomSpecialChar);
       console.log(randomSpecialChar);
+
     }
   }
+
   console.log(arrPrelimPassword);
+
+  //applied array method "join", this takes the elements of an array and joins them together in a string
+  arrPrelimPassword = arrPrelimPassword.join("");
+  console.log(arrPrelimPassword);
+
+  //Created another "for loop" so that the code runs until the charLength requirements are met
+  //Declared a new variable "randomPassword" to store results of loop
+  //Wrote code so that each time the loop runs through the array "arrPrelimPassword" it pushes a randomly selected value to a new array "arrRandomPassword"
+  for (var i = 0; i < charLength; i++) {
+    randomPassword = arrPrelimPassword[Math.floor(Math.random() * arrPrelimPassword.length)];
+    arrRandomPassword.push(randomPassword);
+    console.log(randomPassword);
+  }
+  console.log(arrRandomPassword);
+
+  //applied array method "join", this takes the elements of an array and joins them together in a string
+  //converting array to string; "arrRandomPassword" to "finalPassword"
+  finalPassword = arrRandomPassword.join("");
+  console.log(finalPassword);
+  //needed "return" to end function and output value to text-form section of browser
+  return finalPassword;
 }
-
-
 
